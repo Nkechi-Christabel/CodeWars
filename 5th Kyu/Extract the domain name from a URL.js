833952ed1deca19cb6282f42219743f9b7fa4057
Nkechi Udenkwor <apple@MacBook-Air-2.local>
2022-05-25 23:46:18 +0100

    CodeWars 7 Kyu Solution

--- 5th Kyu/Extract the domain name from a URL.js
+++ 5th Kyu/Extract the domain name from a URL.js
@@ -4,14 +4,11 @@ Write a function that when given a URL as a string, parses out just the domain n
 * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
 * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
 * url = "https://www.cnet.com"                -> domain name = cnet"
-
 */
 
 //Solution
 
 function domainName(url) {
-  //your code here
-
   return url.includes("://")
     ? url.split("://")[1].replace("www.", "").split(".")[0]
     : url.replace("www.", "").split(".")[0];
