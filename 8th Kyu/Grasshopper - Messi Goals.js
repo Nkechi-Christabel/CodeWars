0276e725702db0b64a254fd3abb54026c568b4e8
Nkechi-Christabel <udenkwornkechi@gmail.com>
2022-07-17 02:49:21 +0100

    CodeWars 8 Kyu Solution

---
+++ 8th Kyu/Grasshopper - Messi Goals.js
@@ -0,0 +1,9 @@
+/*
+ */
+
+//Solution
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
---
+++ 8th Kyu/Is it a palindrome?.js
@@ -0,0 +1,9 @@
+/* 
+Write a function that checks if a given string (case insensitive) is a palindrome. 
+*/
+
+//Solution
+
+function isPalindrome(x) {
+  return x.toLowerCase().split("").reverse().join("") === x.toLowerCase();
+}
