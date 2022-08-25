e4f8a3390bfb2aa145b060d647c2f55e3b1642b5
Nkechi-Christabel <udenkwornkechi@gmail.com>
2022-08-25 21:08:11 +0100

    CodeWars 7 Kyu Solution

---
+++ 7th Kyu/Most digits.js
@@ -0,0 +1,14 @@
+/*
+DESCRIPTION:
+Find the number with the most digits.
+
+If two numbers in the argument array have the same number of digits, return the first one in the array.
+*/
+
+//Solution
+
+function findLongest(array) {
+  return array.reduce((a, b) =>
+    `${b}`.length > "" + `${a}`.toString().length ? b : a
+  );
+}
