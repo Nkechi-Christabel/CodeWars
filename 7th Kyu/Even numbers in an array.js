cd640277fede24ecedc294a83dbce78c91d9be9a
Nkechi-Christabel <udenkwornkechi@gmail.com>
2022-08-30 20:12:21 +0100

    CodeWars 7 Kyu Solution

--- 7th Kyu/Even numbers in an array.js
+++ 7th Kyu/Even numbers in an array.js
@@ -8,3 +8,9 @@ For example:
 ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
 ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
 */
+
+//Solution
+
+function evenNumbers(array, number) {
+  return array.filter((n) => !(n % 2)).slice(-number);
+}
