4a5f6a90dc9932ecf658b93bccb600301b53bea8
Nkechi-Christabel <udenkwornkechi@gmail.com>
2022-09-06 21:09:04 +0100

    CodeWars 7 Kyu Solution

--- 7th Kyu/ Sum of numbers from 0 to N.js
+++ 7th Kyu/ Sum of numbers from 0 to N.js
@@ -26,3 +26,24 @@ Output:
 
 0=0
 */
+
+//Solution
+
+var SequenceSum = (function () {
+  function SequenceSum() {}
+
+  SequenceSum.showSequence = function (count) {
+    if (count < 0) return `${count}<0 `;
+    if (count === 0) return `${count}=0`;
+    let sum = 0,
+      arr = [];
+
+    for (let i = 0; i <= count; i++) {
+      sum += i;
+
+      arr.push(i);
+    }
+    return `${arr.join("+")} = ${sum}`;
+  };
+  return SequenceSum;
+})();
