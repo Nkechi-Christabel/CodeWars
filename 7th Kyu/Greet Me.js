65c643fdf5977499b008cc944dc7c7a98ff56040
Nkechi-Christabel <udenkwornkechi@gmail.com>
2022-09-07 21:45:05 +0100

    CodeWars 7 Kyu Solution

---
+++ 7th Kyu/Greet Me.js
@@ -0,0 +1,15 @@
+/*
+DESCRIPTION:
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+*/
+
+//Solution
+
+var greet = function (name) {
+  return `Hello ${name[0].toUpperCase()}${name.toLowerCase().slice(1)}!`;
+};
