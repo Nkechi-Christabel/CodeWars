a704c505f4ab0f2b9c90f4444f633fdee5cffda8
Nkechi-Christabel <udenkwornkechi@gmail.com>
2022-09-12 23:07:13 +0100

    CodeWars 7 Kyu Solution

--- 7th Kyu/Sort the Gift Code.js
+++ 7th Kyu/Sort the Gift Code.js
@@ -8,3 +8,5 @@ Santa asked his organizer to order the characters in alphabetical order, but the
 Sort the Gift Code
 Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
 */
+
+//Solution
