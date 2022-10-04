6efca5af2aca94b99a95b410ad21d4b451e36609
Nkechi-Christabel <udenkwornkechi@gmail.com>
2022-10-05 00:44:49 +0100

    CodeWars 7 Kyu Solution

--- 7th Kyu/ Smallest value of an array.js
+++ 7th Kyu/ Smallest value of an array.js
@@ -6,3 +6,11 @@ Assume the first parameter will always be an array filled with at least 1 number
 min([1,2,3,4,5], 'value') // => 1
 min([1,2,3,4,5], 'index') // => 0
 */
+
+//Solution
+
+function min(arr, toReturn) {
+  return toReturn === "value"
+    ? Math.min(...arr)
+    : arr.indexOf(Math.min(...arr));
+}
