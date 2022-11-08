c4c7b17fae79221f79f6e85549f08924ad081145
Nkechi-Christabel <udenkwornkechi@gmail.com>
2022-11-08 17:42:38 +0100

    CodeWars 8 Kyu Solution

---
+++ 8th Kyu/Sum Mixed Array.js
@@ -0,0 +1,12 @@
+/*
+DESCRIPTION:
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+//Solution
+
+function sumMix(x) {
+  return x.reduce((a, b) => a + +b, 0);
+}
