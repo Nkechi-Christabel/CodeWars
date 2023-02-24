5cd127e6b823d0cadbc6743f211a09b1824db5bd
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-02-24 07:22:22 +0100

    Codewars 7 Kyu Solution

--- 7th Kyu/Greatest common divisor.js
+++ 7th Kyu/Greatest common divisor.js
@@ -3,5 +3,3 @@ Find the greatest common divisor of two positive integers. The integers can be l
 
 The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
 */
-
-//Solution
