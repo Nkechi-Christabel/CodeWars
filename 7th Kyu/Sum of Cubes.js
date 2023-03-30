7aceebb6fc88ec2a40f106859a4cf6825a9c3dfd
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-03-30 12:02:06 +0100

    Codewars 7 Kyu Solution

--- 7th Kyu/Sum of Cubes.js
+++ 7th Kyu/Sum of Cubes.js
@@ -8,3 +8,16 @@ Examples: (Input --> output)
 2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
 3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
 */
+
+//Solution
+
+function sumCubes(n) {
+  return Array.from({ length: n }, (_, i) => i + 1).reduce(
+    (a, b) => a + b ** 3,
+    0
+  );
+
+  //Or
+  ((n * (n + 1)) / 2) ** 2;
+  [...Array(n + 1).keys()].reduce((a, b) => a + b ** 3, 0);
+}
