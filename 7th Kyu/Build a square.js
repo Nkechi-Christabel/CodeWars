ae7f743f1c5771d90558ba9ee79a38647c3b0512
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-03-31 18:21:07 +0100

    Codewars 7 Kyu Solution

--- 7th Kyu/Build a square.js
+++ 7th Kyu/Build a square.js
@@ -12,5 +12,5 @@ n = 3, so I expect a 3x3 square back just like below as a string:
 //Solution
 
 function generateShape(integer) {
-  return [...Array(integer).keys()].map((_) => "+".repeat(integer)).join("\n");
+  return [...Array(integer)].map((_) => "+".repeat(integer)).join("\n");
 }
