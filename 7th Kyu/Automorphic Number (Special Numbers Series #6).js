e7367cfbd759175441471b7f871963b517dc2465
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-05-18 05:15:30 +0100

    Codewars 7 Kyu Solution

---
+++ 7th Kyu/Automorphic Number (Special Numbers Series #6).js
@@ -0,0 +1,8 @@
+/*
+A number is called Automorphic number if and only if its square ends in the same digits as the number itself.
+
+Task
+Given a number determine if it Automorphic or not 
+*/
+
+//Solution
