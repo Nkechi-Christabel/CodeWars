98fc4a1dd9f1fc0764699f33397b8f838b046518
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-02-03 18:15:47 +0100

    Codewars 7 Kyu Solution

---
+++ 7th Kyu/ Naughty or Nice.js
@@ -0,0 +1,32 @@
+/*
+Santa is coming to town and he needs your help finding out who's been naughty or nice.
+You will be given an entire year of JSON data following this format:
+
+{
+    January: {
+        '1': 'Naughty','2': 'Naughty', ..., '31': 'Nice'
+    },
+    February: {
+        '1': 'Nice','2': 'Naughty', ..., '28': 'Nice'
+    },
+    ...
+    December: {
+        '1': 'Nice','2': 'Nice', ..., '31': 'Naughty'
+    }
+}
+Your function should return "Naughty!" or "Nice!" depending on the total number of occurrences in a given year
+(whichever one is greater). If both are equal, return "Nice!"
+*/
+
+//Solution
+function naughtyOrNice(data) {
+  let naughty = 0,
+    nice = 0;
+  for (const month in data) {
+    for (const key in data[month]) {
+      if (data[month][key] === "Naughty") naughty++;
+      else nice++;
+    }
+  }
+  return naughty > nice ? "Naughty!" : "Nice!";
+}
